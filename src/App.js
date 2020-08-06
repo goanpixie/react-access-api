bbd277484a7d364f099cc99986d445ec07a8de51
Priyanka R Shah <priyankarshah@Priyankas-MacBook-Pro.local>
2020-08-06 15:25:29 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState,useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
+
+const gitHubUrl = "https://jsonplaceholder.typicode.com/comments?postId=1";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  //userData is an object initialized to empty objecta nd setUserData is update function for userData object.
+  const [userData, setUserData] = useState([])
+
+  //causes side effect after the component renders, its called after each render function and component update. Having an empty array prevents it being called after each render
+  useEffect(()=>{
+    gitHubAxiosGetUserData()
+  },[])
+  
+  //async function helps runs js code in synchronous thread, while returning a promise
+  //the fetch function does a network request to fetch the url and return a Promise that resolves into an response object, which is parsed using json()
+  const gitHubAxiosGetUserData =async()=>{
+    const response = await axios.get(gitHubUrl);
+    setUserData(response.data)
+  }
+
+  return(
+    <div>
+      <header>Github User data</header>
+      <div>
+        <ul>
+            {userData.map((listValue)=>{
+              return (
+              <div>
+                <li><strong><u>{(listValue).name}</u></strong></li>
+                <p>{(listValue).email}</p>
+                <p>{(listValue).body}</p>
+              </div>
+                      )
+            })}
+        </ul>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
